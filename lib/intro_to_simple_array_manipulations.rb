395e7a1cc1789ac5cc54289d47bf21830a4c6183
Jason Fournier <jmf4nier@hotmail.com>
2019-01-31 20:25:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,11 +27,11 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, element)
-
+  array.insert(3, element)
 end
 
 def using_uniq(array)
-
+  array.uniq
 end
 
 def using_flatten(array)
